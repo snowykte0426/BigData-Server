69e84c8f1e8848aaa9455ba8c487ef4d83fee187
KIM TAEEUN <snowykte0426@naver.com>
2025-03-25 16:20:12 +0900

    add: enable asynchronous processing in MinsoleApplication

--- src/main/java/com/snowykte0426/minsole/MinsoleApplication.java
+++ src/main/java/com/snowykte0426/minsole/MinsoleApplication.java
@@ -2,8 +2,10 @@ package com.snowykte0426.minsole;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class MinsoleApplication {
 
     public static void main(String[] args) {
