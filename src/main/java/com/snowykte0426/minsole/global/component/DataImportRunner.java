4f5aa7762c94828faa6445f00f888c4934671c5e
KIM TAEEUN <snowykte0426@naver.com>
2025-04-03 00:43:01 +0900

    remove: comment out DataImportRunner class and its implementation

--- src/main/java/com/snowykte0426/minsole/global/component/DataImportRunner.java
+++ src/main/java/com/snowykte0426/minsole/global/component/DataImportRunner.java
@@ -5,18 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-@Component
-public class DataImportRunner implements CommandLineRunner {
-
-    private final XlsxDataImporter importer;
-
-    @Autowired
-    public DataImportRunner(XlsxDataImporter importer) {
-        this.importer = importer;
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        importer.importData("output.xlsx");
-    }
-}
+//@Component
+//public class DataImportRunner implements CommandLineRunner {
+//
+//    private final XlsxDataImporter importer;
+//
+//    @Autowired
+//    public DataImportRunner(XlsxDataImporter importer) {
+//        this.importer = importer;
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        importer.importData("output.xlsx");
+//    }
+//}
